dfa6ba2873db29d4ba64bedca25234dba293594a
luke-h1 <luke.howsam@yahoo.com>
2020-10-11 21:37:56 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views/');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose 😀'));
+
+app.use('/', indexRouter); // root route
+
+indexRouter;
+
+app.listen(process.env.PORT || 3000);
+// routes = controllers
+// in node most people refer to controllers as routes but they're the same thing
+// models = database models
